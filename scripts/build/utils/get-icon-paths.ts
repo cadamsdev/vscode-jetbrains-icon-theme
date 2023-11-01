57a5fbe91b4978787c3daea32a013bf935cfb760
Arthur Fontaine <57626613+arthur-fontaine@users.noreply.github.com>
2023-11-02 00:46:04 +0100

    refactor: add explicit return type
    Co-authored-by: Chad Adams <cadamsdev@outlook.com>

--- scripts/build/utils/get-icon-paths.ts
+++ scripts/build/utils/get-icon-paths.ts
@@ -2,7 +2,7 @@ import * as path from "node:path";
 
 import { Theme } from "../types/Theme";
 
-export function getIconPaths(theme: Theme, themePath = "") {
+export function getIconPaths(theme: Theme, themePath = ""): string[] {
   return Object.values(theme.iconDefinitions).map((iconDefinition) =>
     path.join(path.dirname(themePath), iconDefinition.iconPath),
   );
